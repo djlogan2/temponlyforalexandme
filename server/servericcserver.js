170e1273382854834b73fcdf199f065c15a8be7f
Serhii Romanov <serhiiromanov@pop-os.localdomain>
2021-12-01 14:37:27 +0200

    remove all ts-generated js files

--- server/servericcserver.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var servericcserver_1 = require("../imports/server/servericcserver");
-global.ICCServer = new servericcserver_1.default();
