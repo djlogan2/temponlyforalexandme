29943825fc3a51a9d7aa6468ccb869843aa4bd36
Serhii Romanov <serhiiromanov@pop-os.localdomain>
2021-12-10 11:45:37 +0200

    make insertion of chat work

--- imports/server/serverchat.ts
+++ imports/server/serverchat.ts
@@ -1,8 +1,11 @@
 import { Meteor } from "meteor/meteor";
 import { check } from "meteor/check";
 import ServerICCServer from "./servericcserver";
+import CommonChat from "../commonchat";
 
 declare const ICCServer: ServerICCServer;
+
+export default class ServerChat extends CommonChat {}
 
 Meteor.publish("chat", (chatId) => {
   check(chatId, String);
