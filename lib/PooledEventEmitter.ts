4fbb4164c7c34ecb5789d1de24703318b66dd5db
djlogan2 <djlogan@chessclub.com>
2021-12-28 08:23:47 -0700

    ok, another round of tests, and working on the global event emitter system on the client side for subscriptions

--- lib/PooledEventEmitter.ts
+++ lib/PooledEventEmitter.ts
@@ -5,26 +5,49 @@ export default abstract class PooledEventEmitter {
 
     protected poolname: string;
 
+    /**
+     * The name of the pool. In this class, it is for informational purposes only
+     * @param{string} poolname
+     * @protected
+     */
     protected constructor(poolname: string) {
         this.poolname = poolname;
     }
 
+    /**
+     * This returns an emitter connected to this pool, so that when people call ".on", ".off" and ".removeListeners", this class instance will be notified when the first one signs up and the last one cleans up.
+     * @return{ICCEventEmitter} ICCEventEmitter
+     */
     public newEmitter(): ICCEventEmitter {
         return ICCEventEmitter.getNew(this);
     }
 
+    /**
+     * This is not for public use. The emitters will call this.
+     */
     public addActiveEmitter(): void {
         if (!this.count) this.onFirstEvent();
         this.count += 1;
     }
 
+    /**
+     * This is not for public use. The emitters will call this.
+     */
     public removeActiveEmitter(): void {
         this.count -= 1;
         if (!this.count) this.onLastEvent();
     }
 
+    /**
+     * Called when the first emitter signs up for the first events. Subsequent events, and even subsequent emitters, will NOT trigger this method.
+     * @protected
+     */
     protected abstract onFirstEvent(): void;
 
+    /**
+     * Called when the last emitter removes the last listener. If this method is invoked, there are no emitters listening to any events in this pool.
+     * @protected
+     */
     protected abstract onLastEvent(): void;
 }
 
