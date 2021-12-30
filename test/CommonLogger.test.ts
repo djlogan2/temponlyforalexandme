ad3e876a5861e10d84e08e298a96ce84c488e934
djlogan2 <djlogan@chessclub.com>
2021-12-29 21:42:49 -0700

    CommmonLogger tested and documented - except for the data part. It needs to be tested, and I'm just not sure it works. I think we should just take it out.

--- test/CommonLogger.test.ts
+++ test/CommonLogger.test.ts
@@ -12,6 +12,8 @@ class TestCommonLogger extends CommonLogger {
         this.logLevelChanged(l);
     }
 
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     protected writeTolog(level: LOGLEVEL, message: string, data?: unknown, userid?: string): void {
         this.writeToLogCalled = true;
     }
@@ -28,6 +30,7 @@ describe("CommonLogger", function() {
                     it(`should ${rl <= ll ? "" : "not "}write a log entry when log level is ${loggerlevel} and requested level is ${requestedlevel}`, function() {
                         const logger = new TestCommonLogger(loggerlevel as LOGLEVEL);
                         let messageMethodCalled = false;
+                        // @ts-ignore
                         logger[requestedlevel].call(logger, () => {messageMethodCalled = true; return "message";});
                         expect(logger.writeToLogCalled).to.equal(rl <= ll);
                         expect(messageMethodCalled).to.equal(rl <= ll);
@@ -41,42 +44,3 @@ describe("CommonLogger", function() {
         });
     });
 });
-//    private log(level: LOGLEVEL, message: () => string, data?: unknown, userid?: string) {
-//         if (!this.writable(level)) return;
-//         this.writeTolog(level, message(), data, userid);
-//     }
-//
-//     protected abstract writeTolog(
-//         level: LOGLEVEL,
-//         message: string,
-//         data?: unknown,
-//         userid?: string
-//     ): void;
-//
-//     protected logLevelChanged(newloglevel: LOGLEVEL) {
-//         this.identlevel = newloglevel;
-//     }
-//
-//     public fatal(message: () => string, data?: unknown, userid?: string): void {
-//         this.log("fatal", message, data, userid);
-//     }
-//
-//     public error(message: () => string, data?: unknown, userid?: string): void {
-//         this.log("error", message, data, userid);
-//     }
-//
-//     public warn(message: () => string, data?: unknown, userid?: string): void {
-//         this.log("warn", message, data, userid);
-//     }
-//
-//     public info(message: () => string, data?: unknown, userid?: string): void {
-//         this.log("info", message, data, userid);
-//     }
-//
-//     public debug(message: () => string, data?: unknown, userid?: string): void {
-//         this.log("debug", message, data, userid);
-//     }
-//
-//     public trace(message: () => string, data?: unknown, userid?: string): void {
-//         this.log("trace", message, data, userid);
-//     }
